4763b768f7fe1c5c2eac7bd86b37989be0b4e5ff
Artem Liskovshchenko <artem140742@gmail.com>
2024-07-18 17:20:37 +0300

    check

--- src/server.js
+++ src/server.js
@@ -7,35 +7,35 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 
-const PORT = Number(env('PORT', '3001'));
+const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
-    
+
     const app = express();
 
     app.use(cors());
-    
+
     app.use(express.json());
-    
+
     app.use(pino({
         transport: {
             target: 'pino-pretty',
         },
     })
         ,);
-    
+
     app.use(cookieParser());
 
     app.use(rootRouter);
-    
+
     app.use('*', notFoundHandler);
-    
+
     app.use(errorHandler);
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
-    
+
 };
 
 
